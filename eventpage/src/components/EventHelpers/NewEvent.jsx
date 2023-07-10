61e68dd2e531df104426f123cffb23500cddb591
Frieda Haringsma <fharingsma@hotmail.com>
2023-07-10 13:47:48 +0200

    mobile verion new and update done

--- eventpage/src/components/EventHelpers/NewEvent.jsx
+++ eventpage/src/components/EventHelpers/NewEvent.jsx
@@ -72,51 +72,53 @@ export const NewEvent = () => {
   //console.log(users, categories);
   // form to create new event
   return (
-    <div className="form-container">
-      <h1>Create new event</h1>
+    <section className="form-page">
+      <section className="form-container">
+        <h1>Create new event</h1>
 
-      <div className="form-field">
-        <Form method="post" id="new-event-form" action="/event/new">
-          <label>Title:</label>
-          <input type="text" name="title" placeholder="Title" />
-          <label>Description:</label>
-          <input type="text" name="description" placeholder="Description" />
-          <label>Image(URL):</label>
-          <input
-            placeholder="https://website.com/image.jpg......."
-            aria-label="image"
-            type="text"
-            name="image"
-          />
-          <label>Location</label>
-          <input type="text" name="location" placeholder="Location" />
-          <label>Select categorie:</label>
-          <select name="categoryIds" placeholder="Select categorie">
-            <option value="default">Choose a Categorie</option>
-            {categories.map((categorie) => (
-              <option key={categorie.id} value={categorie.id}>
-                {categorie.name}
-              </option>
-            ))}
-          </select>
-          <label> Starts at:</label>
-          <input type="datetime-local" name="startTime" />
-          <label>Ends at:</label>
-          <input type="time" name="endTime" />
-          <label>Select User</label>
-          <select name="createdBy" placeholder="Select user">
-            <option value="default">Choose a User</option>
-            {users.map((user) => (
-              <option key={user.id} value={user.id}>
-                {user.name}
-              </option>
-            ))}
-          </select>
-          <button className="form-btn" type="submit">
-            Save
-          </button>
-        </Form>
-      </div>
-    </div>
+        <section className="form-field">
+          <Form method="post" id="new-event-form" action="/event/new">
+            <label>Title:</label>
+            <input type="text" name="title" placeholder="Title" />
+            <label>Description:</label>
+            <input type="text" name="description" placeholder="Description" />
+            <label>Image(URL):</label>
+            <input
+              placeholder="https://website.com/image.jpg......."
+              aria-label="image"
+              type="text"
+              name="image"
+            />
+            <label>Location</label>
+            <input type="text" name="location" placeholder="Location" />
+            <label>Select categorie:</label>
+            <select name="categoryIds" placeholder="Select categorie">
+              <option value="default">Choose a Categorie</option>
+              {categories.map((categorie) => (
+                <option key={categorie.id} value={categorie.id}>
+                  {categorie.name}
+                </option>
+              ))}
+            </select>
+            <label> Starts at:</label>
+            <input type="datetime-local" name="startTime" />
+            <label>Ends at:</label>
+            <input type="time" name="endTime" />
+            <label>Select User</label>
+            <select name="createdBy" placeholder="Select user">
+              <option value="default">Choose a User</option>
+              {users.map((user) => (
+                <option key={user.id} value={user.id}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+            <button className="form-btn" type="submit">
+              Save
+            </button>
+          </Form>
+        </section>
+      </section>
+    </section>
   );
 };
